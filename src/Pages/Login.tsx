f3fabbd3e45db0a5329c47f657fe3ae3121c5d7c
Janderff <janderffigueiredo@gmail.com>
2025-07-25 19:42:34 -0300

    feat(ValidadeLogin):Inserido o login para rota aluno e feito a responsividade

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -30,9 +30,13 @@ export function Login() {
       const user = users.find(
         (u: any) => u.email === email && u.password === password
       )
-      if (user) {
+      if (user && user.role === 'professor') {
         setTimeout(() => {
           navigate('/teacherDashboard')
+        }, 1000)
+      } else if (user && user.role === 'aluno') {
+        setTimeout(() => {
+          navigate('/studentDashboard')
         }, 1000)
       } else {
         console.log('Login falhou: Credenciais inválidas.')
@@ -43,17 +47,21 @@ export function Login() {
   }
 
   return (
-    <div className='w-full h-screen flex items-center justify-center bg-[#FFF3F0] '>
-      <div className='flex-row w-[540px] h-[545px] justify-center items-center shadow-2xl shadow-gray-400 border-2 bg-[#FFFFFF] border-white rounded-xl'>
+    <div className='w-full h-screen flex items-center justify-center bg-[#FFF3F0] flex-col lg:flex-row p-4 overflow-y-auto'>
+      <div className='w-full max-w-[540px] h-auto p-6 justify-center items-center shadow-2xl shadow-gray-400 border-2 bg-[#FFFFFF] border-white rounded-xl'>
         <div className=' w-full h-[250px] flex items-center justify-center'>
-          <img src={BjjTutorLogo} alt='Logo do BJJ Tutors Academy' />
+          <img
+            src={BjjTutorLogo}
+            alt='Logo do BJJ Tutors Academy'
+            className='max-w-[350px] w-full h-auto'
+          />
         </div>
         <div className='mt-3 flex flex-col items-center'>
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className='flex flex-col gap-6'
+            className='flex flex-col gap-6 w-full max-w[450px] px-4'
           >
-            <div className='grid  gap-3 w-[450px]'>
+            <div className=' gap-3 w-full max-w-[450px] px-4 grid '>
               <Label>E-MAIL</Label>
               <Input {...register('email')} />
               {formState.errors.email?.message && (
@@ -69,7 +77,11 @@ export function Login() {
                   {formState.errors.password?.message}
                 </span>
               )}
-              <Button type='submit' className='mt-2' variant='destructive'>
+              <Button
+                type='submit'
+                className='mt-2 w-full sm:w-auto '
+                variant='destructive'
+              >
                 ENTRAR
               </Button>
             </div>
--- src/Routes/AppRoutes.tsx
+++ src/Routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from '../Pages/Login'
 import { TeacherDashboard } from '@/Pages/TeacherDashboard'
+import { StudentDashboard } from '@/Pages/StudentDashboard'
 
 export function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ export function AppRoutes() {
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/teacherDashboard' element={<TeacherDashboard />} />
+        <Route path='/studentDashboard' element={<StudentDashboard />} />
       </Routes>
     </BrowserRouter>
   )
